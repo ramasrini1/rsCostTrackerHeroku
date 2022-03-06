a3f0e193a0905afe066b864243df5bdeff51d6a5
ramasrini1 <ramasrini1@gmail.com>
2022-03-06 12:20:49 -0800

    testing

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
-#db.create_all()
+db.create_all()
 
 ##############################################################################
 # User signup/login/logout
